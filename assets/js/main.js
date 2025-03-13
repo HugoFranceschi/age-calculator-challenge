f419aafe6cce2b76c16e915bf415cdd71e1bcce4
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-03-13 17:21:27 +0100

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-// Coder ici
+const inputDay = document.getElementById("dayIn");
+const inputMouth = document.getElementById("monthIn");
+const inputYear = document.getElementById("yearIn");
+
+let spanYear = document.getElementById("yearOut");
+let spanMouth = document.getElementById("monthOut");
+let spanDay = document.getElementById("dayOut");
+
+const buttonCalculat = document.getElementById("calculateBtn");
+
+let jour = new Date();
+
+inputDay.addEventListener("input", () => {
+	const valeur = inputDay.value;
+
+	console.log(valeur);
+});
