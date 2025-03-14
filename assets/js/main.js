6b8a0e2199cc961f8a42067a0aea634268fbbcd2
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-03-14 15:38:55 +0100

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,61 @@ let spanDay = document.getElementById("dayOut");
 
 const buttonCalculat = document.getElementById("calculateBtn");
 
-let jour = new Date();
-
 inputDay.addEventListener("input", () => {
-	const valeur = inputDay.value;
+	const valeurDay = Number(inputDay.value);
 
-	console.log(valeur);
+	if (valeurDay >= 1 && valeurDay <= 31) {
+		console.log(valeurDay);
+	} else {
+		inputDay.value = "";
+	}
+});
+
+inputMouth.addEventListener("input", () => {
+	const valeurMouth = Number(inputMouth.value);
+
+	if (valeurMouth >= 1 && valeurMouth <= 12) {
+		console.log(valeurMouth);
+	} else {
+		inputMouth.value = "";
+	}
+});
+
+inputYear.addEventListener("input", () => {
+	const valeurYear = Number(inputYear.value);
+
+	if (valeurYear >= 1 && valeurYear <= 2025) {
+		console.log(valeurYear);
+	} else {
+		inputYear.value = "";
+	}
+});
+
+buttonCalculat.addEventListener("click", () => {
+	// let jour =
+	// 	(new Date() - new Date(inputYear.value, inputMouth.value, inputDay.value)) /
+	// 	(1000 * 60 * 60 * 24);
+
+	let jour = new Date().getDate() - inputDay.value;
+
+	spanDay.textContent = Math.floor(jour);
+	console.log(Math.floor(jour));
+
+	// let mois =
+	// 	(new Date() - new Date(inputYear.value, inputMouth.value, inputDay.value)) /
+	// 	(1000 * 60 * 60 * 24);
+
+	let mois = new Date().getMonth() + 1 - inputMouth.value;
+
+	spanMouth.textContent = Math.floor(mois);
+	console.log(Math.floor(mois));
+
+	let anne =
+		(new Date() - new Date(inputYear.value, inputMouth.value, inputDay.value)) /
+		(1000 * 60 * 60 * 24);
+
+	anne = anne / 365;
+
+	spanYear.textContent = Math.floor(anne);
+	console.log(Math.floor(anne));
 });
